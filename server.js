ed5a7d978e39823f10bb0e2f1baaf999288ba767
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-08-11 00:11:42 +0300

    adds mongoose and connects MongoDB (db-contacts .contacts)

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// require("dotenv").config();
+// console.log(process.env.DB_URI);
+
+// FirbcEKhLKxkkor3;
+
+const mongoose = require("mongoose");
+
+// const DB_URI =
+//   "mongodb+srv://BogdanGrynjuk:FirbcEKhLKxkkor3@cluster0.fzazmdh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+const DB_URI = process.env.DB_URI;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_URI)
+  .then(() =>
+    app.listen(3000, () => console.log("Database connection successful"))
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
