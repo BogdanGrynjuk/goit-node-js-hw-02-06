a4b9b2283e5095efb2e118439ffd9ade9dae4c94
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-08-11 01:13:01 +0300

    updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const app = require("./app");
 
-// require("dotenv").config();
-// console.log(process.env.DB_URI);
-
-// FirbcEKhLKxkkor3;
-
 const mongoose = require("mongoose");
-
-// const DB_URI =
-//   "mongodb+srv://BogdanGrynjuk:FirbcEKhLKxkkor3@cluster0.fzazmdh.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
 const DB_URI = process.env.DB_URI;
 
