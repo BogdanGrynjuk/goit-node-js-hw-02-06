f0e16fc16bd49b2d706b4efa3ff1f0499a4419dc
BogdanGrynjuk <bgrynjuk@ukr.net>
2023-08-17 22:35:05 +0300

    adds pagination for the collection of contacts and logic for filter contacts by favorite field

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,7 +3,24 @@ const { ctrlWrapper } = require("../../helpers");
 
 const getAllContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const contacts = await Contact.find({ owner }).exec();
+  const { page = 1, limit = 20, favorite } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const contacts = favorite
+    ? await Contact.find({ owner, favorite }, "-createdAt -updatedAt", {
+        skip,
+        limit,
+      })
+        .populate("owner", "name email")
+        .exec()
+    : await Contact.find({ owner }, "-createdAt -updatedAt", {
+        skip,
+        limit,
+      })
+        .populate("owner", "name email")
+        .exec();
+
   res.json({
     status: "Success",
     code: res.statusCode,
